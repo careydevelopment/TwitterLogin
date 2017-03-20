00145b9c2964028940820f1017b9532b040f6ab3
bcarey <bcarey@LAPTOP-9IJC4N3L>
2017-03-20 06:09:21 -0400

    More web framework

--- src/main/java/com/careydevelopment/twitterlogin/controller/TwitterLoginController.java
+++ src/main/java/com/careydevelopment/twitterlogin/controller/TwitterLoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TwitterLoginController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/twitterLogin")
 	public String twiterLogin(Model model) {
 		return "twitterLogin";
 	}
